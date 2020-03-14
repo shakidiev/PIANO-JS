fc507cd23783885ed5c2d31f9e3e55a41a8221e2
shakidiev <60289817+shakidiev@users.noreply.github.com>
2020-03-15 00:47:57 +0300

    piano

---
+++ script/javascript.js
@@ -0,0 +1,13 @@
+let keys = document.querySelectorAll(".key");
+
+keys.forEach(key => {
+  key.addEventListener("click", playNote);
+});
+
+function playNote(e) {
+  let key = e.target;
+  let note = document.getElementById(key.dataset.note);
+  key.classList.add("active");
+  note.currentTime = 0;
+  note.play();
+}
